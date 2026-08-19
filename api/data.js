3748e41fa272676d9c6dc6ae3a9a1a1a22d6d4e3
Iyad khattab <shaikhonahmed200r@gmail.com>
2026-08-19 07:43:54 +0300

    Fix Vercel API deployment

--- api/data.js
+++ api/data.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-export default function handler(req, res) {
+module.exports = (req, res) => {
     try {
         const contentPath = path.join(process.cwd(), "content.json");
         const content = JSON.parse(fs.readFileSync(contentPath, "utf8"));
@@ -9,8 +9,9 @@ export default function handler(req, res) {
         res.status(200).json(content);
     } catch (error) {
         console.error(error);
+
         res.status(500).json({
             error: "Could not load portfolio content"
         });
     }
-}
+};
